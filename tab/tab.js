17efea0876e20103e1fdef07e457f3ad80e8df9c
AAABBBCCCAAAA <AAABBBCCCAAAA@protonmail.com>
2022-02-03 20:05:05 +0100

    fix color of the link

--- tab/tab.js
+++ tab/tab.js
@@ -6,15 +6,16 @@ const stats = getStats();
 
 for(let i = 0; i < stats.highestStats.length; i ++) {
     const stat = stats.highestStats[i];
-    const percent = document.createElement("td");
     const tr = document.createElement("tr");
+    const percent = document.createElement("td");
     const site = document.createElement("td");
     const data = document.createElement("td");
     const network = document.createElement("td");
+    tr.className = "oneResult";
     percent.textContent = stat.percent;
     site.textContent = stat.origin;
-    data.textContent = (statsStorage.bytesDataCenter[stat.origin] * 0.000001).toFixed(2);
-    network.textContent = ((statsStorage.bytesNetwork[stat.origin] + statsStorage.bytesDataCenter[stat.origin]) * 0.000001).toFixed(2);
+    data.textContent = toMegaByte(statsStorage.bytesDataCenter[stat.origin]).toFixed(2);
+    network.textContent = toMegaByte(statsStorage.bytesNetwork[stat.origin] + statsStorage.bytesDataCenter[stat.origin]).toFixed(2);
     tr.appendChild(percent);
     tr.appendChild(site);
     tr.appendChild(data);
